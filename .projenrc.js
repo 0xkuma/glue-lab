d1186c2eefe351d0279b304518582d4b56407217
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-11-03 23:17:09 +0800

    Initial Commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,18 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.50.0',
   defaultReleaseBranch: 'main',
   name: 'glue-lab',
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  workflowGitIdentity: false,
+  github: false,
+  gitignore: ['.env'],
+  deps: ['dotenv'], /* Runtime dependencies of this module. */
+  tsconfig: {
+    compilerOptions: {
+      lib: ['es2020'],
+      target: 'es2020',
+    },
+  },
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
