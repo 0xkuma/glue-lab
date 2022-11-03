367032e5677a75cebb600d14889aafbb164f74f1
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-11-03 23:18:09 +0800

    Add glue & role

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,47 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as glue from 'aws-cdk-lib/aws-glue';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const role = new iam.Role(this, 'Role', {
+      roleName: 'GlueRole',
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+    });
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [],
+        resources: ['*'],
+      }),
+    );
+
+    const jobCommandProperty: glue.CfnJob.JobCommandProperty = {
+      name: 'poc-glue',
+      pythonVersion: '3',
+      scriptLocation: '',
+    };
+    new glue.CfnJob(this, 'poc-glue', {
+      name: 'poc-glue',
+      timeout: 300,
+      maxRetries: 0,
+      glueVersion: '3.0',
+      command: jobCommandProperty,
+      role: role.roleArn,
+      workerType: 'G.1X',
+      numberOfWorkers: 2,
+      defaultArguments: {
+        '--TempDir': '',
+        '--enable-metrics': '',
+        '--enable-continuous-cloudwatch-log': 'true',
+        '--enable-continuous-log-filter': 'true',
+        '--job-bookmark-option': 'job-bookmark-disable',
+        '---enable-spark-ui': 'false',
+      },
+    });
   }
 }
 
@@ -18,6 +54,5 @@ const devEnv = {
 const app = new App();
 
 new MyStack(app, 'glue-lab-dev', { env: devEnv });
-// new MyStack(app, 'glue-lab-prod', { env: prodEnv });
 
 app.synth();
