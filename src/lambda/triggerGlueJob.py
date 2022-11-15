616fe72af11d9c0174659ef2e039f3e88053c9e4
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-11-15 22:28:13 +0800

    Remove deprecated parameter

--- src/lambda/triggerGlueJob.py
+++ src/lambda/triggerGlueJob.py
@@ -8,7 +8,6 @@ job_name = os.environ['JOB_NAME']
 def lambda_handler(event, context):
     glue.start_job_run(
         JobName=job_name,
-        AllocatedCapacity=123,
         Timeout=120,
         WorkerType='G.1X',
         NumberOfWorkers=2,
